5e5599a4f04ce41d3fc33857d3909cf4bf120a4c
DariaStarintseva <yudina.daria@inbox.ru>
2023-09-05 17:27:35 +0300

    3.1.3

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -3,7 +3,6 @@ package ru.kata.spring.boot_security.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.model.Role;
-
 import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 
 import java.util.List;
@@ -13,12 +12,12 @@ public class RoleService {
 private final RoleRepository roleRepository;
 @Autowired
     public RoleService(RoleRepository roleRepository) {
+
         this.roleRepository = roleRepository;
     }
 
     public List<Role> getAllRoles() {
-
-    return roleRepository.findAll();
+        return roleRepository.findAll();
     }
 
 }
